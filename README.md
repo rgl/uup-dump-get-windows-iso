fd237055bb22df332aacacc9d685e8bea4587b41
Rui Lopes <rgl@ruilopes.com>
2024-02-18 12:35:43 +0000

    upgrade to windows 11 23h2

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This must be executed in a Windows 2022 host.
 
 This supports the following Windows Editions:
 
-* `windows-11`: Windows 11 22000 (aka 21H2) Enterprise
+* `windows-11`: Windows 11 22631 (aka 23H2) Enterprise
 * `windows-2022`: Windows Server 2022 20348 (aka 21H2) Standard
 
 **NB** The Windows Server 2019 iso source files are not available in the Unified Update Platform (UUP) and cannot be downloaded by UUP dump.
--- uup-dump-get-windows-iso.ps1
+++ uup-dump-get-windows-iso.ps1
@@ -18,7 +18,7 @@ $TARGETS = @{
     # see https://en.wikipedia.org/wiki/Windows_11
     # see https://en.wikipedia.org/wiki/Windows_11_version_history
     "windows-11" = @{
-        search = "cumulative update windows 11 22000 amd64" # aka 21H2. Enterprise EOL: October 8, 2024.
+        search = "windows 11 22631 amd64" # aka 23H2. Enterprise EOL: November 10, 2026.
         editions = @("Professional")
     }
     # see https://en.wikipedia.org/wiki/Windows_Server_2022
